3877af4f3bb2d55606ae850545e02b3e8fb8760d
adclose <adclose@gmail.com>
2020-06-04 21:06:41 -0600

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = open("README.md", "r").read()
 
 setuptools.setup(
     name="segy_lite",
-    version="0.0.1",
+    version="0.0.2",
     author="Aaron Close",
     author_email="adclose@gmail.com",
     description="Lightweight SEGY Reading and Plotting Tool",
